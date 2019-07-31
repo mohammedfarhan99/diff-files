ab9171f7c3cc0636879e664d4228ae7d2ee11616
mohammedfarhan99 <31024609+mohammedfarhan99@users.noreply.github.com>
2019-07-31 14:29:48 +0530

    Create compare.sh

---
+++ compare.sh
@@ -0,0 +1,10 @@
+echo "Comparing files 1a and 1b"
+diff $WORKSPACE/SetA/1a.json $WORKSPACE/SetB/1b.json
+echo "Comparing files 2a and 2b"
+diff $WORKSPACE/SetA/2a.json $WORKSPACE/SetB/2b.json
+echo "Comparing files 3a and 3b"
+diff $WORKSPACE/SetA/3a.json $WORKSPACE/SetB/3b.json
+echo "Comparing files 4a and 4b"
+diff $WORKSPACE/SetA/4a.json $WORKSPACE/SetB/4b.json
+echo "Comparing files 5a and 5b"
+diff $WORKSPACE/SetA/5a.json $WORKSPACE/SetB/5b.json
